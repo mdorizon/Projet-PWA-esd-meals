774e1ad5795e99f75861e43bb4a2b5391f500942
MDorizon <mdorizon@proton.me>
2024-11-20 13:22:54 +0100

    :sparkles: tabs

--- app/meals/(tabs)/_layout.tsx
+++ app/meals/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from "expo-router";
 import { useEffect } from "react";
 import { StatusBar } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function MealsLayout() {
 
@@ -10,8 +11,21 @@ export default function MealsLayout() {
 
   return (
     <Tabs>
-      <Tabs.Screen name="random" options={{ title: "Random Meal" }} />
-      <Tabs.Screen name="index" options={{ title: "List Meal" }} />
+      <Tabs.Screen 
+        name="random" 
+        options={{ 
+          title: "Random Meal",
+          tabBarIcon: ({ focused }) => <Ionicons name={focused ? 'dice' : 'dice-outline'} size={24} color={"#ff5478"}/>,
+          tabBarActiveTintColor: '#ff5478',
+        }} 
+      />
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          title: "List Meal",
+          tabBarIcon: ({ focused }) => <Ionicons name={focused ? 'list' : 'list-outline'} size={24} color={"#ff5478"}/>,
+          tabBarActiveTintColor: '#ff5478',
+          }} />
     </Tabs>
   )
 }
